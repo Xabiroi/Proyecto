15d4e82e5f48542af4618efc8ab97adb80622643
Xabiroi <xsarri9@opendeusto.es>
2017-01-23 17:31:58 +0100

    Teoricamente pathfinding arreglado

--- Proyecto/src/BD/UnidadBDTest.java
+++ Proyecto/src/BD/UnidadBDTest.java
@@ -23,9 +23,10 @@ public class UnidadBDTest {
 		u.setNombre("Soldado");
 		UnidadBD[][] tabla=Ventanas.Partida.crearTablero();
 		tabla[0][0]=u;
-		try{
 		tabla=u.AlgoritmoPathfinding(u.getDistancia(), 0, 0, 6, 6, tabla);
-		assertEquals(tabla[6][6].getNombre(),"Soldado");}catch(NullPointerException e){}
+		try{
+		assertEquals(tabla[6][6].getNombre(),null);}catch(NullPointerException e){}
+		assertEquals(tabla[0][0].getNombre(),"Soldado");
 	}
 	@Test
 	public void test2() {
@@ -35,6 +36,16 @@ public class UnidadBDTest {
 		tabla[0][0]=u;
 		tabla=u.AlgoritmoPathfinding(u.getDistancia(), 0, 0, 2, 3, tabla);
 		assertEquals(tabla[2][3].getNombre(),"Soldado");
+	}
+	//Desaparicion de unidades
+	@Test
+	public void test3() {
+		SoldadoRaso u=new SoldadoRaso(31,1);
+		u.setNombre("Soldado");
+		UnidadBD[][] tabla=Ventanas.Partida.crearTablero();
+		tabla[31][1]=u;
+		tabla=u.AlgoritmoPathfinding(u.getDistancia(), 31, 2, 31, 1, tabla);
+		assertEquals(tabla[31][1].getNombre(),"Soldado");
 	}
 
 }
